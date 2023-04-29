86829e420b79d2603f68c99a8e72be7570670a11
Denis Sleptsov <hurrdurrrderp@gmail.com>
2023-04-29 21:51:41 +0300

    Tree menu template rendering up to 4 levels

--- templatetags/draw_menu.py
+++ templatetags/draw_menu.py
@@ -1,8 +1,43 @@
 from django import template
 
+from ..models import TreeMenu
+
 register = template.Library()
 
 
 @register.inclusion_tag('menu.html')
 def draw_menu(menu_name):
-    return {'menu_name': menu_name}
+    query_set = TreeMenu.objects.filter(name=menu_name)
+    if len(query_set) == 0:
+        return {}
+    tree_menu = query_set[0]
+    menu_nodes = tree_menu.root_node.descendants(include_self=True)
+    # return {'menu_nodes': menu_nodes}
+    return {
+        "menu": [{
+            "text": "Напитки",
+            "url": "/drinks",
+            "is_selected": True,
+            "children": [
+                {
+                    "text": "Кофе",
+                    "url": "/drinks/coffee",
+                },
+                {
+                    "text": "Чай",
+                    "url": "/drinks/tea",
+                    "is_selected": True,
+                    "children": [
+                        {
+                            "text": "Зелёный",
+                            "url": "/drinks/tea/green"
+                        },
+                        {
+                            "text": "Чёрный",
+                            "url": "/drinks/tea/black"
+                        }
+                    ]
+                }
+            ]
+        }]
+    }
