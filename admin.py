7294c12f04342ca956d1ed9ed228d2d5074b5756
Denis Sleptsov <hurrdurrrderp@gmail.com>
2023-04-29 18:35:15 +0300

    TreeMenu model

--- admin.py
+++ admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import MenuNode
+from .models import MenuNode, TreeMenu
 
 admin.site.register(MenuNode)
+admin.site.register(TreeMenu)
--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from .django_tree_queries.tree_queries.models import TreeNode
 
 
 class MenuNode(TreeNode):
-    url = models.CharField(max_length=500)
+    url = models.CharField(max_length=500, blank=True)
     is_named_url = models.BooleanField()
     text = models.CharField(max_length=100)
 
@@ -11,6 +11,9 @@ class MenuNode(TreeNode):
         return self.text
 
 
-class Menu(models.Model):
+class TreeMenu(models.Model):
     root_node = models.ForeignKey(MenuNode, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
