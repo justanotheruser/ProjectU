db38fedfc3db637f5b3a920f6302eedac3805b4d
Denis Sleptsov <hurrdurrrderp@gmail.com>
2023-04-29 15:35:02 +0300

    Models for tree menus

--- admin.py
+++ admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MenuNode
+
+admin.site.register(MenuNode)
--- models.py
+++ models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from tree_queries.models import TreeNode
 
-# Create your models here.
+
+class MenuNode(TreeNode):
+    url = models.CharField(max_length=500)
+    is_named_url = models.BooleanField()
+    text = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.text
+
+
+class Menu(models.Model):
+    root_node = models.ForeignKey(MenuNode, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
