7dadebbbc244f7ba63c49a11d09b85e193899857
Denis Sleptsov <hurrdurrrderp@gmail.com>
2023-04-30 14:33:37 +0300

    Convert tree list into dict

--- templatetags/draw_menu.py
+++ templatetags/draw_menu.py
@@ -1,8 +1,41 @@
 from django import template
 
-from ..models import TreeMenu
+from ..models import TreeMenu, MenuNode
 
 register = template.Library()
+
+
+def node2dict(node: MenuNode):
+    return {
+        "text": node.text,
+        "url": node.url,
+        "is_selected": False
+    }
+
+
+def listed_tree2dict(tree_list: list[MenuNode]):
+    node_dict = node2dict(tree_list[0])
+    if len(tree_list) == 1:
+        return node_dict
+    child_depth = tree_list[0].tree_depth + 1
+    subtrees = []
+    subtree_list = None
+    for node in tree_list[1:]:
+        if node.tree_depth == child_depth:
+            if subtree_list is not None:
+                subtrees.append(subtree_list)
+            subtree_list = [node]
+        else:
+            subtree_list.append(node)
+    subtrees.append(subtree_list)
+    node_dict['children'] = [listed_tree2dict(subtree_list) for subtree_list in subtrees]
+    return node_dict
+
+
+def convert_menu_for_template(root: MenuNode):
+    menu_nodes = root.descendants(include_self=True)
+    menu_dict = listed_tree2dict(menu_nodes)
+    return menu_dict.get('children', [])
 
 
 @register.inclusion_tag('menu.html')
@@ -11,33 +44,4 @@ def draw_menu(menu_name):
     if len(query_set) == 0:
         return {}
     tree_menu = query_set[0]
-    menu_nodes = tree_menu.root_node.descendants(include_self=True)
-    # return {'menu_nodes': menu_nodes}
-    return {
-        "menu": [{
-            "text": "Напитки",
-            "url": "/drinks",
-            "is_selected": True,
-            "children": [
-                {
-                    "text": "Кофе",
-                    "url": "/drinks/coffee",
-                },
-                {
-                    "text": "Чай",
-                    "url": "/drinks/tea",
-                    "is_selected": True,
-                    "children": [
-                        {
-                            "text": "Зелёный",
-                            "url": "/drinks/tea/green"
-                        },
-                        {
-                            "text": "Чёрный",
-                            "url": "/drinks/tea/black"
-                        }
-                    ]
-                }
-            ]
-        }]
-    }
+    return {'menu': convert_menu_for_template(tree_menu.root_node)}
--- tests.py
+++ tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import MenuNode
+from .templatetags.draw_menu import listed_tree2dict
+
+
+class ListedTree2DictTest(TestCase):
+    def test_root_only(self):
+        root = MenuNode.objects.create(url='root', is_named_url=False, text='root')
+        result = listed_tree2dict(root.descendants(include_self=True))
+        self.assertEqual({'text': 'root', 'url': 'root', 'is_selected': False}, result)
+
+    def test_one_level_one_child(self):
+        root = MenuNode.objects.create(url='root', is_named_url=False, text='root')
+        ch1 = MenuNode.objects.create(parent=root, url='some url', is_named_url=False, text='some text')
+        result = listed_tree2dict(root.descendants(include_self=True))
+        self.assertEqual({'text': 'root', 'url': 'root', 'is_selected': False,
+                          'children': [{'text': 'some text', 'url': 'some url', 'is_selected': False}]}, result)
+
+    def test_one_level_multiple_children(self):
+        root = MenuNode.objects.create(url='root', is_named_url=False, text='root')
+        ch1 = MenuNode.objects.create(parent=root, url='one', is_named_url=False, text='one')
+        ch2 = MenuNode.objects.create(parent=root, url='two', is_named_url=False, text='two')
+        result = listed_tree2dict(root.descendants(include_self=True))
+        self.assertEqual({'text': 'root', 'url': 'root', 'is_selected': False,
+                          'children': [
+                              {'url': 'one', 'text': 'one', 'is_selected': False},
+                              {'url': 'two', 'text': 'two', 'is_selected': False}]}, result)
+
+    def test_three_levels(self):
+        root = MenuNode.objects.create(url='root', is_named_url=False, text='root')
+        ch1 = MenuNode.objects.create(parent=root, url='one', is_named_url=False, text='one')
+        ch_1_1 = MenuNode.objects.create(parent=ch1, url='one_one', is_named_url=False, text='one_one')
+        ch_1_2 = MenuNode.objects.create(parent=ch1, url='one_two', is_named_url=False, text='one_two')
+        ch2 = MenuNode.objects.create(parent=root, url='two', is_named_url=False, text='two')
+        ch2_1 = MenuNode.objects.create(parent=ch2, url='two_one', is_named_url=False, text='two_one')
+        ch2_1_1 = MenuNode.objects.create(parent=ch2_1, url='two_one_one', is_named_url=False,
+                                          text='two_one_one')
+        result = listed_tree2dict(root.descendants(include_self=True))
+        self.assertEqual({'text': 'root', 'url': 'root', 'is_selected': False, 'children': [
+            {'text': 'one', 'url': 'one', 'is_selected': False,
+             'children': [{'text': 'one_one', 'url': 'one_one', 'is_selected': False},
+                          {'text': 'one_two', 'url': 'one_two', 'is_selected': False}]},
+            {'text': 'two', 'url': 'two', 'is_selected': False, 'children': [
+                {'text': 'two_one', 'url': 'two_one', 'is_selected': False,
+                 'children': [{'text': 'two_one_one', 'url': 'two_one_one', 'is_selected': False}]}]}]}, result)
